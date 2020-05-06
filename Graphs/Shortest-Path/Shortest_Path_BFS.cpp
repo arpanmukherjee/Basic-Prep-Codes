117dbb57e8ac8862ed2ff854a2188286ea39857f
Arpan Mukherjee <arpan17007@iiitd.ac.in>
2020-05-06 16:53:55 +0530

    Minor updates

--- Graphs/Shortest-Path/Shortest_Path_BFS.cpp
+++ Graphs/Shortest-Path/Shortest_Path_BFS.cpp
@@ -4,30 +4,15 @@ using namespace std;
 #define MAX 100005
 #define INF 0x3f3f3f3f
 
+int V, E;
 bool vis[MAX];
 int dist[MAX];
 vector<int> adj[MAX];
 
-int main()
+void bfsShortestPath(int src)
 {
-    int V, E, x, y, i, src;
+    int i, x, y;
     queue<int> Q;
-    memset(dist, INF, sizeof(dist));
-    memset(vis, false, sizeof(vis));
-
-    cout << "Enter the no of Nodes";
-    cin >> V;
-    cout << "Enter the no of Edges";
-    cin >> E;
-    for(i = 0 ; i < E ; i++)
-    {
-        cin >> x >> y;
-        adj[x].push_back(y);
-        adj[y].push_back(x);
-    }
-    cout << "Enter the source node:";
-    cin >> src;
-
     Q.push(src);
     dist[src] = 0;
     while (!Q.empty())
@@ -48,5 +33,26 @@ int main()
     cout << "Distance from the source node " << src << endl;
     for(i = 0 ; i < V ; i++)
         cout << i << "\t" << dist[i] << endl;
+}
+
+int main()
+{
+    int x, y, i, src;
+    memset(dist, INF, sizeof(dist));
+    memset(vis, false, sizeof(vis));
+
+    cout << "Enter the no of Nodes";
+    cin >> V;
+    cout << "Enter the no of Edges";
+    cin >> E;
+    for(i = 0 ; i < E ; i++)
+    {
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    cout << "Enter the source node:";
+    cin >> src;
+    bfsShortestPath(src);
     return 0;
 }
